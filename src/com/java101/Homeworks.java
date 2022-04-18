5a878d930f07a24109d40eb0c11c23ae5a1bcbed
ibrahimmutlu <60941439+ibrahimmutlu@users.noreply.github.com>
2022-04-18 12:33:42 +0300

    Update Homeworks.java

--- src/com/java101/Homeworks.java
+++ src/com/java101/Homeworks.java
@@ -219,3 +219,55 @@ public class Manav {
 }
 
 */
+
+
+// Hesap Makinesi
+/*
+import java.util.Scanner;
+
+public class Hesapmakinesi {
+
+    public static void main(String[]args){
+
+        Scanner inp=new Scanner(System.in);
+
+        int n1,n2,select;
+
+        System.out.println("ilk sayiyi giriniz:");
+
+        n1=inp.nextInt();
+
+        System.out.println("ikinci sayiyi giriniz:");
+
+        n2=inp.nextInt();
+
+        System.out.println("1-Toplama\n2-Cikarma\n3-Carpma\n4-Bolme");
+
+        System.out.println("Lutfen seciminizi yapiniz:");
+
+        select=inp.nextInt();
+
+        switch (select){
+            case 1:
+                System.out.println("Toplam:"+(n1+n2));
+                break;
+            case 2:
+                System.out.println("Cikarma:"+(n1-n2));
+                break;
+            case 3:
+                System.out.println("Carpma:"+(n1*n2));
+                break;
+            case 4:
+                if(n2==0){
+                    System.out.println("bir sayi sifira bolunmez.");
+                }
+                System.out.println("Bolme:" +(n1/n2));
+                break;
+            default:
+                System.out.println("Hatali giris yaptiniz. Tekrar deneyiniz:");
+        }
+
+    }
+}
+
+*/
