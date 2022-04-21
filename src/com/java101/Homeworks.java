993e25962d3f08602b9297e12959f052f6e68513
ibrahimmutlu <60941439+ibrahimmutlu@users.noreply.github.com>
2022-04-21 18:52:46 +0300

    Update Homeworks.java

--- src/com/java101/Homeworks.java
+++ src/com/java101/Homeworks.java
@@ -441,3 +441,44 @@ public class Havasicakliginagöreetkinlik {
     }
 }
 
+
+//KÜÇÜKTEN BÜYÜĞE SIRALAMA
+import java.util.Scanner;
+
+public class Kücüktenbüyüge {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int a, b, c;
+
+        System.out.print("1. sayiyi giriniz:");
+        a = input.nextInt();
+
+        System.out.print("2.sayiyi giriniz:");
+        b = input.nextInt();
+
+        System.out.print("3.sayiyi giriniz:");
+        c = input.nextInt();
+
+        if ((a < b) && (a < c)) {
+            if (b < c) {
+                System.out.print("a<b<c");
+            } else {
+                System.out.print("a<c<b");
+            }
+        }  else if ((b < a) && (b < c)) {
+            if (a < c) {
+                System.out.print("b<a<c");
+            } else {
+                System.out.print("b<c<a");
+            }
+        } else  {
+            if (a < b) {
+                System.out.print("c<a<b");
+            } else {
+                System.out.print("c<b<a");
+            }
+        }
+    }
+}
